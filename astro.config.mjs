89977990d4418d4874a7bb5fb4e7d9de8b71fa9c
wandeber <berthewanderer@gmail.com>
2024-03-10 10:57:08 +0100

    Tailwind added. Some aliases added. Several minor fixes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import preact from "@astrojs/preact";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.wande.dev",
-  integrations: [preact()]
+  integrations: [preact(), tailwind()]
 });
--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,4 +1,4 @@
-document.addEventListener('astro:page-load', () => {
+document.addEventListener('astro:page-load', function () {
 	document.querySelector('.hamburger').addEventListener('click', function () {
 		document.querySelector('.nav-links').classList.toggle('expanded');
 	});
